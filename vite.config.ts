c6d36d984b126abbc364abee9c6d7eecd977cc3a
lakkutlakrishna <lakkutlakrishna@gmail.com>
2025-07-13 10:27:40 +0530

    Ecommerce-using-react

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'build', // Ensure output directory is 'build' for gh-pages
+    chunkSizeWarningLimit: 1000, // Suppress chunk size warnings
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
