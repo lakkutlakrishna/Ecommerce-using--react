ec06f522c3f462cbbea4d558b8edf64b1e37939e
lakkutlakrishna <lakkutlakrishna@gmail.com>
2025-07-13 10:40:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Ecommerce-using--react/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
